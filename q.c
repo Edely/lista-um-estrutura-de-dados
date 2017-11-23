9e6ed251d65b910c0006dae71f1e8c0f463a94df
edelygomes <edelygomes@gmail.com>
2017-11-22 21:42:03 -0300

    q aceitando

--- q.c
+++ q.c
@@ -6,18 +6,23 @@ int f91(int inteiro);
 int main(int argc, char *argv[]){
     
     int argumento, n;
-    scanf("%d", &argumento);  
-    n = f91(argumento);
-    printf("%i", n);
+    argumento = -1;
+    while(argumento!=0){
+        scanf("%d", &argumento);
+        if(argumento !=0){
+            n = f91(argumento);        
+            printf("f91(%i) = %i\n", argumento, n);
+        }
+    }
+    
     return 0;
 }
 
 int f91(int inteiro){    
     if(inteiro >= 101){
         inteiro = inteiro - 10;
-        return inteiro;
     }else{
         inteiro = f91(f91(inteiro + 11));
-        return inteiro;
     }        
+    return inteiro;
 }
